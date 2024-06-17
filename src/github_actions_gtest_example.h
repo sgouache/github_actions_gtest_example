80966cf9c05b8efd0883132f6452c5333db43e67
Stephane Gouache <stephane.gouache@orange.com>
2024-06-17 19:36:57 +0200

    Fix export

--- src/github_actions_gtest_example.h
+++ src/github_actions_gtest_example.h
@@ -2,10 +2,23 @@
 
 #include <iostream>
 
+#if defined(__unix__) || defined(__unix) || \
+    (defined(__APPLE__) && defined(__MACH__))
+#define __unix_or_mac__
+#else
+#define __windows__
+#endif
+
+#ifdef __unix_or_mac__ 
+#define VISIBLE __attribute__((visibility("default")))
+#else
+/* Windows Visual C++ only */
+#define VISIBLE __declspec(dllexport)
+#endif
 
 namespace githubActionsGtestExample {
 
-  std::string sayHello();
-  int add(int _first, int _second);
+  VISIBLE std::string sayHello();
+  VISIBLE int add(int _first, int _second);
 
 } // namespace githubActionsGtestExample
