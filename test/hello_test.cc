4bad8484773783e83a756985aed05b0287130e57
Stephane Gouache <stephane.gouache@orange.com>
2024-06-18 11:35:18 +0200

    Try testing functions from DLL

--- test/hello_test.cc
+++ test/hello_test.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include "github_actions_gtest_example.h"
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -6,4 +7,14 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(HelloTest, CallHello) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", githubActionsGtestExample::sayHello().c_str());
+}
+
+TEST(HelloTest, CallAdd) {
+  // Expect addition to be correct.
+  EXPECT_EQ(2, githubActionsGtestExample::add(1, 1));
 }
